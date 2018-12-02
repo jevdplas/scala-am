3343af3f60881bb1f64e88c006a19fa8baa3532c
Jens Van der Plas <jevdplas@vub.be>
2018-12-02 13:34:30 +0100

    Continue working on Swap! TODO: fix infinite loop in ConcurrentAAM.

--- src/scalaam/scala/language/atomlang/AtomlangSemantics.scala
+++ src/scalaam/scala/language/atomlang/AtomlangSemantics.scala
@@ -31,12 +31,13 @@ class AtomlangSemantics[A <: Address, V, T, C, TID <: ThreadIdentifier](addressA
       * @param store The store to use for the evaluation.
       * @param t     The current timestamp.
       */
-    override def stepEval(e: SchemeExp, env: Env, store: Sto, t: T) = e match {
+    override def stepEval(e: SchemeExp, env: Env, store: Sto, t: T): Actions = e match {
         case AtomlangDeref(exp, _) => Push(FrameDeref(), exp, env, store)
         case AtomlangFuture(_, _) =>
             val tid = tidAllocator.allocate(e, t)
             val tidv = future(tid)
             NewFuture(tid, tidv, e, env, store) // Let the machine handle the actual thread creation.
+        case AtomlangSwap(atomExp, funExp, argExps, _) => Push(FrameSwapAtom(atomExp, funExp, argExps, env), atomExp, env, store)
         case _ => super.stepEval(e, env, store, t)
     }
     
@@ -48,7 +49,7 @@ class AtomlangSemantics[A <: Address, V, T, C, TID <: ThreadIdentifier](addressA
       * @param store The store to continue with.
       * @param t     The current timestamp.
       */
-    override def stepKont(v: V, frame: Frame, store: Sto, t: T) = frame match {
+    override def stepKont(v: V, frame: Frame, store: Sto, t: T): Actions = frame match {
         case FrameDeref() => // Todo: extend to atoms.
             val futures = getFutures(v)
             if (futures.isEmpty) {
@@ -56,9 +57,24 @@ class AtomlangSemantics[A <: Address, V, T, C, TID <: ThreadIdentifier](addressA
             } else {
                 futures.map(tid => Action.DerefFuture(tid, store))
             }
+        case FrameSwapAtom(atomExp, funExp, argExps, env) => Push(FrameSwapFun(v, atomExp, funExp, argExps, env), funExp, env, store)
+        case FrameSwapFun(atomv, atomExp, funExp, argExps, env) => swapArgs(atomv, atomExp, v, funExp, List(), argExps, env, store, t)
+        case FrameSwapArgs(atomv, atomExp, funv, funExp, args, argExp :: rest , env) => swapArgs(atomv, atomExp, funv, funExp, (argExp, v) :: args, rest, env, store, t)
         case _ => super.stepKont(v, frame, store, t)
     }
     
-    case class FrameDeref() extends SchemeFrame
+    def swapArgs(atomv: V, atomExp: SchemeExp, funv: V, funExp: SchemeExp, args: List[(SchemeExp, V)], toEval: List[SchemeExp], env: Env, store: Sto, t: T): Actions = toEval match {
+        case Nil => evalSwap(atomv, atomExp, funv, funExp, args, env, store, t)
+        case argExp :: _ => Push(FrameSwapArgs(atomv, atomExp, funv, funExp, args, toEval, env), argExp, env, store)
+    }
     
+    def evalSwap(atomv: V, atomExp: SchemeExp, funv: V, funExp: SchemeExp, args: List[(SchemeExp, V)], env: Env, store: Sto, t: T): Actions = {
+        ???
+    }
+    
+    trait AtomLangFrame extends SchemeFrame
+    case class FrameDeref() extends AtomLangFrame
+    case class FrameSwapAtom(atomExp: SchemeExp, funExp: SchemeExp, argExps: List[SchemeExp], env: Env) extends AtomLangFrame
+    case class FrameSwapFun(atomv: V, atomExp: SchemeExp, funExp: SchemeExp, argExps: List[SchemeExp], env: Env) extends AtomLangFrame
+    case class FrameSwapArgs(atomv: V, atomExp: SchemeExp, funv: V, funExp: SchemeExp, args: List[(SchemeExp, V)], toEval: List[SchemeExp], env: Env) extends SchemeFrame
 }
