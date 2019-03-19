038ce64e33f99277dbf6498b4cc8745a73ec8a87
Jens Van der Plas <jevdplas@vub.be>
2019-03-19 15:22:18 +0100

    Fix address bug.

--- test/Atomlang/Concurrent/simple.scm
+++ test/Atomlang/Concurrent/simple.scm
@@ -1,8 +1,8 @@
 ;; Example based on Dynamic Partial Order Reduction paper
 (let* ((x 0)
        (y 0)
-       (t1 (future (begin (set! x 1) (set! x 2))))
-       (t2 (future (begin (set! y 1) (set! x 3)))))
+       (t1 (future (set! x 1) (set! x 2)))
+       (t2 (future (set! y 1) (set! x 3))))
  (deref t1)
  (deref t2)
  (and (= y 1)
