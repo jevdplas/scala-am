a41aada8b484a8bff3318a5a28ea6ef64d791060
Jens Van der Plas <jevdplas@vub.be>
2019-06-07 23:13:15 +0200

    Merge branch Reuse in branch Thesis.

---
+++ src/scalaam/scala/bench/BenchReuse.scala
@@ -0,0 +1,78 @@
+package scala.bench
+
+import java.io.{BufferedWriter, FileWriter}
+import java.text.SimpleDateFormat
+import java.util.{Calendar, Date}
+
+import au.com.bytecode.opencsv.CSVWriter
+import scalaam.StandardPrelude
+import scalaam.bench.BenchConfig.Prelude._
+import scalaam.bench.BenchConfig._
+import scalaam.core._
+import scalaam.graph.DotGraph
+import scalaam.language.atomlang.{AtomlangParser, AtomlangSemantics}
+import scalaam.language.scheme.{MakeSchemeLattice, SchemeExp}
+import scalaam.lattice.Type
+
+import scala.machine.IncrementalConcurrentModular
+
+object BenchReuse extends App {
+    
+    val address   = NameAddress
+    val tid       = ConcreteTID
+    val timestamp = ZeroCFA[SchemeExp]()
+    val lattice   = new MakeSchemeLattice[SchemeExp, address.A, Type.S,  Type.B, Type.I, Type.R, Type.C, Type.Sym]
+    val sem       = new AtomlangSemantics[address.A, lattice.L, timestamp.T, SchemeExp, tid.threadID](address.Alloc[timestamp.T, SchemeExp], tid.Alloc())
+    val modInc    = new IncrementalConcurrentModular[SchemeExp, address.A, lattice.L, timestamp.T, tid.threadID](StoreType.BasicStore, sem, tid.Alloc())
+    val graph     = DotGraph[modInc.State, modInc.Transition]()
+    
+    val now:                Date =  Calendar.getInstance().getTime
+    val format: SimpleDateFormat = new SimpleDateFormat("_yyyy-MM-dd-HH'h'mm")
+    val output:           String = "./Results_ReuseInc" + format.format(now) + ".csv"
+    val fields:           String = "Benchmark,tid,(iteration,number)*" // Field names for the csv file.
+    
+    val    out = new BufferedWriter(new FileWriter(output))
+    val writer = new CSVWriter(out)
+    
+    def forFile(file: String, atPrelude: Prelude): Unit = {
+        try {
+            val f = scala.io.Source.fromFile(file)
+            // Add the necessary preludes to the file contents.
+            val content: String = StandardPrelude.atomlangPrelude ++ (atPrelude match {
+                case Prelude.lock => lockPrelude
+                case Prelude.list => listPrelude
+                case Prelude.none => ""
+            }) ++ f.getLines.mkString("\n")
+            f.close()
+            val name = file.split("/").last.dropRight(4) // DropRight removes ".scm".
+            val program: SchemeExp = AtomlangParser.parse(content)
+            val to = Timeout.seconds(timeout) // Start timer.
+            modInc.runWithLabels[graph.G](program, to) // Run benchmark.
+            val sc = to.time // Seconds passed.
+            val re = sc > timeout // Check whether timeout has occurred.
+            if (re) {
+                val line: List[Any] = List(name, "timed out.")
+                display("timeout")
+                writer.writeNext(line.mkString(","))
+                writer.flush()
+            } else {
+                val labels = modInc.theLabels
+                labels.foreach{case (int, mp) =>
+                    val line: List[Any] = name :: int :: mp.toList
+                    display("\n" ++ name ++ "\t" ++ line.tail.mkString(","))
+                    writer.writeNext(line.mkString(","))
+                    writer.flush()
+                }
+            }
+        } catch {
+            case e: Throwable => e.printStackTrace()
+        }
+    }
+    
+    writer.writeNext(fields)
+    writer.flush()
+    display("Benchmark,tid,(iteration,number)*")
+    benchmarks.foreach(Function.tupled(forFile))
+    writer.close()
+    display("\n\n***** Finished *****\n")
+}
--- src/scalaam/scala/machine/IncrementalConcurrentModular.scala
+++ src/scalaam/scala/machine/IncrementalConcurrentModular.scala
@@ -23,90 +23,92 @@ class IncrementalConcurrentModular[Exp, A <: Address, V, T, TID <: ThreadIdentif
     type StateReadDeps  = Map[  A, Set[State]]
     type StateWriteDeps = Map[  A, Set[State]]
     
+    var theLabels: List[(Int, Map[Int, Int])] = List()
+    
+    /** Class collecting the dependencies of all threads. */
+    case class Deps(joined: StateJoinDeps, read: StateReadDeps, written: StateWriteDeps)
+    
+    /** Class containing bookkeeping information for the inner loop of a thread. All arguments except the first three are optional. */
+    case class InnerLoopState(work: List[State], store: VStore, results: RetVals, visited: Set[State] = Set.empty,
+                              result: V = lattice.bottom, created: Created = Set.empty, effects: Effects = Set.empty,
+                              deps: Deps = Deps(Map.empty.withDefaultValue(Set.empty), Map.empty.withDefaultValue(Set.empty),
+                                  Map.empty.withDefaultValue(Set.empty)),
+                              edges: UnlabeledEdges = Map.empty) extends SmartHash
+    
+    /** Class containing bookkeeping information for the outer loop. Contains a.o. the global store. */
+    case class OuterLoopState(threads: Threads, work: List[State], deps: Deps, results: RetVals, store: VStore, edges: Edges) extends SmartHash
+    
+    /** Innerloop like ConcurrentModular.run.innerLoop, except that now relations between effects and states are tracked. */
+    @scala.annotation.tailrec
+    final def innerLoop(timeout: Timeout.T, iState: InnerLoopState): InnerLoopState = {
+        if (timeout.reached || iState.work.isEmpty) return iState
+        innerLoop(timeout, iState.work.foldLeft(iState.copy(work = List())){case (iStateAcc, curState) =>
+            if (iStateAcc.visited.contains(curState)) iStateAcc
+            else {
+                val StepResult(successors, created, result, effects, store: VStore) = curState.step(iStateAcc.store, iStateAcc.results)
+                val (read, written, joined) = effects.foldLeft((iStateAcc.deps.read, iStateAcc.deps.written, iStateAcc.deps.joined))
+                {case (acc@(r, w, j), eff) => eff match {
+                    case     JoinEff(tid: TID@unchecked) => (r, w, j + (tid -> (j(tid) + curState)))
+                    case  ReadAddrEff(addr: A@unchecked) => (r + (addr -> (r(addr) + curState)), w, j)
+                    case WriteAddrEff(addr: A@unchecked) => (r, w + (addr -> (w(addr) + curState)), j)
+                    case _ => acc
+                }
+                }
+                val vis = if (store.asInstanceOf[DeltaStore[A, V]].updated.nonEmpty) Set.empty[State] else iStateAcc.visited + curState
+                InnerLoopState(iStateAcc.work ++ successors, store.asInstanceOf[DeltaStore[A, V]].clearUpdated, iStateAcc.results, vis,
+                    lattice.join(iStateAcc.result, result.getOrElse(lattice.bottom)),
+                    iStateAcc.created ++ created, iStateAcc.effects ++ effects,
+                    Deps(joined, read, written),
+                    // iStateAcc.edges + (curState -> (iStateAcc.edges.getOrElse(curState, Set.empty) ++ successors)))
+                    iStateAcc.edges + (curState -> successors))
+            }
+        })
+    }
+    
+    /**
+      * OuterLoop like ConcurrentModular.run.outerLoop, but upon reanalysis, a thread is not started from scratch again.
+      * Added: iteration counter for edge annotation.
+      */
+    @scala.annotation.tailrec
+    final def outerLoop(timeout: Timeout.T, oState: OuterLoopState, iteration: Int = 1): OuterLoopState = {
+        if (timeout.reached || oState.work.isEmpty) return oState
+        outerLoop(timeout, oState.work.foldLeft(oState.copy(work = List())){case (oStateAcc, curState) =>
+            val stid: TID = curState.tid
+            val iState = innerLoop(timeout, InnerLoopState(List(curState), oStateAcc.store, oStateAcc.results))
+            // Some new threads may have been spawned.
+            val (todoCreated, newThreads): (Set[State], Threads) = iState.created.foldLeft((Set[State](), oStateAcc.threads)) {case ((createdAcc, threadsAcc), curState) =>
+                if (threadsAcc(curState.tid).contains(curState)) (createdAcc, threadsAcc) // There already is an identical thread, so do nothing.
+                else (createdAcc + curState, threadsAcc + (curState.tid -> (threadsAcc(curState.tid) + curState)))
+            }
+            // Add the newly found dependencies.
+            val readDeps  = oStateAcc.deps.read    ++ iState.deps.read
+            val writeDeps = oStateAcc.deps.written ++ iState.deps.written
+            val joinDeps  = oStateAcc.deps.joined  ++ iState.deps.joined
+            // Based on R/W and W/W conflicts, decide on the states that need re-evaluation.
+            val todoEffects: List[State] = iState.deps.written.keySet.foldLeft(List[State]())((acc, addr) =>
+                if (oStateAcc.store.lookup(addr) == iState.store.lookup(addr)) acc else acc ++ readDeps(addr).filter(_.tid != stid) ++ writeDeps(addr).filter(_.tid != stid))
+            // Calculate the thread's new return value. If it differs, some other threads joining this thread need re-evaluation.
+            val retVal: V = lattice.join(oStateAcc.results(stid), iState.result)
+            val todoJoined: Set[State] = if (oStateAcc.results(stid) == retVal) Set.empty else joinDeps(stid)
+            val fromInterference: Set[State] = todoJoined ++ todoEffects // Use a set to suppress duplicates.
+            OuterLoopState(newThreads, oStateAcc.work ++ todoCreated ++ fromInterference, Deps(joinDeps, readDeps, writeDeps),
+                // All outgoing edges of states that need recomputation (are in fromInterference) are removed. Each edge that is added is annotated with the iteration number.
+                oStateAcc.results + (stid -> retVal), iState.store, oStateAcc.edges -- fromInterference ++ iState.edges.mapValues(set => set.map((BaseTransition(iteration.toString), _))))
+        }, iteration + 1)
+    }
+    
     override def run[G](program: Exp, timeout: Timeout.T)(implicit ev: Graph[G, State, Transition]): G = {
-    
-        /** Class collecting the dependencies of all threads. */
-        case class Deps(joined: StateJoinDeps, read: StateReadDeps, written: StateWriteDeps)
-    
-        /** Class containing bookkeeping information for the inner loop of a thread. All arguments except the first three are optional. */
-        case class InnerLoopState(work: List[State], store: VStore, results: RetVals, visited: Set[State] = Set.empty,
-                                  result: V = lattice.bottom, created: Created = Set.empty, effects: Effects = Set.empty,
-                                  deps: Deps = Deps(Map.empty.withDefaultValue(Set.empty), Map.empty.withDefaultValue(Set.empty),
-                                      Map.empty.withDefaultValue(Set.empty)),
-                                  edges: UnlabeledEdges = Map.empty) extends SmartHash
-        
-        /** Class containing bookkeeping information for the outer loop. Contains a.o. the global store. */
-        case class OuterLoopState(threads: Threads, work: List[State], deps: Deps, results: RetVals, store: VStore, edges: Edges) extends SmartHash
-    
-        /** Innerloop like ConcurrentModular.run.innerLoop, except that now relations between effects and states are tracked. */
-        @scala.annotation.tailrec
-        def innerLoop(iState: InnerLoopState): InnerLoopState = {
-            if (timeout.reached || iState.work.isEmpty) return iState
-            innerLoop(iState.work.foldLeft(iState.copy(work = List())){case (iStateAcc, curState) =>
-                if (iStateAcc.visited.contains(curState)) iStateAcc
-                else {
-                    val StepResult(successors, created, result, effects, store: VStore) = curState.step(iStateAcc.store, iStateAcc.results)
-                    val (read, written, joined) = effects.foldLeft((iStateAcc.deps.read, iStateAcc.deps.written, iStateAcc.deps.joined))
-                        {case (acc@(r, w, j), eff) => eff match {
-                            case     JoinEff(tid: TID@unchecked) => (r, w, j + (tid -> (j(tid) + curState)))
-                            case  ReadAddrEff(addr: A@unchecked) => (r + (addr -> (r(addr) + curState)), w, j)
-                            case WriteAddrEff(addr: A@unchecked) => (r, w + (addr -> (w(addr) + curState)), j)
-                            case _ => acc
-                            }
-                        }
-                    val vis = if (store.asInstanceOf[DeltaStore[A, V]].updated.nonEmpty) Set.empty[State] else iStateAcc.visited + curState
-                    InnerLoopState(iStateAcc.work ++ successors, store.asInstanceOf[DeltaStore[A, V]].clearUpdated, iStateAcc.results, vis,
-                                   lattice.join(iStateAcc.result, result.getOrElse(lattice.bottom)),
-                                   iStateAcc.created ++ created, iStateAcc.effects ++ effects,
-                                   Deps(joined, read, written),
-                                   // iStateAcc.edges + (curState -> (iStateAcc.edges.getOrElse(curState, Set.empty) ++ successors)))
-                                   iStateAcc.edges + (curState -> successors))
-                }
-            })
-        }
-    
-        /**
-          * OuterLoop like ConcurrentModular.run.outerLoop, but upon reanalysis, a thread is not started from scratch again.
-          * Added: iteration counter for edge annotation.
-          */
-        @scala.annotation.tailrec
-        def outerLoop(oState: OuterLoopState, iteration: Int = 1): OuterLoopState = {
-            if (timeout.reached || oState.work.isEmpty) return oState
-            outerLoop(oState.work.foldLeft(oState.copy(work = List())){case (oStateAcc, curState) =>
-                val stid: TID = curState.tid
-                val iState = innerLoop(InnerLoopState(List(curState), oStateAcc.store, oStateAcc.results))
-                // Some new threads may have been spawned.
-                val (todoCreated, newThreads): (Set[State], Threads) = iState.created.foldLeft((Set[State](), oStateAcc.threads)) {case ((createdAcc, threadsAcc), curState) =>
-                    if (threadsAcc(curState.tid).contains(curState)) (createdAcc, threadsAcc) // There already is an identical thread, so do nothing.
-                    else (createdAcc + curState, threadsAcc + (curState.tid -> (threadsAcc(curState.tid) + curState)))
-                }
-                // Add the newly found dependencies.
-                val readDeps  = oStateAcc.deps.read    ++ iState.deps.read
-                val writeDeps = oStateAcc.deps.written ++ iState.deps.written
-                val joinDeps  = oStateAcc.deps.joined  ++ iState.deps.joined
-                // Based on R/W and W/W conflicts, decide on the states that need re-evaluation.
-                val todoEffects: List[State] = iState.deps.written.keySet.foldLeft(List[State]())((acc, addr) =>
-                    if (oStateAcc.store.lookup(addr) == iState.store.lookup(addr)) acc else acc ++ readDeps(addr).filter(_.tid != stid) ++ writeDeps(addr).filter(_.tid != stid))
-                // Calculate the thread's new return value. If it differs, some other threads joining this thread need re-evaluation.
-                val retVal: V = lattice.join(oStateAcc.results(stid), iState.result)
-                val todoJoined: Set[State] = if (oStateAcc.results(stid) == retVal) Set.empty else joinDeps(stid)
-                val fromInterference: Set[State] = todoJoined ++ todoEffects // Use a set to suppress duplicates.
-                OuterLoopState(newThreads, oStateAcc.work ++ todoCreated ++ fromInterference, Deps(joinDeps, readDeps, writeDeps),
-                    // All outgoing edges of states that need recomputation (are in fromInterference) are removed. Each edge that is added is annotated with the iteration number.
-                    oStateAcc.results + (stid -> retVal), iState.store, oStateAcc.edges -- fromInterference ++ iState.edges.mapValues(set => set.map((BaseTransition(iteration.toString), _))))
-            }, iteration + 1)
-        }
     
         /** Filters out unreachable graph components that may result from invalidating edges. */
         @scala.annotation.tailrec
-        def findConnectedStates(work: List[State], edges: Edges, visited: Set[State] = Set.empty, acc: GraphEdges = List.empty): GraphEdges = {
+        def findConnectedStates(timeout: Timeout.T, work: List[State], edges: Edges, visited: Set[State] = Set.empty, acc: GraphEdges = List.empty): GraphEdges = {
             if (timeout.reached || work.isEmpty) return acc
-            if (visited.contains(work.head)) findConnectedStates(work.tail, edges, visited, acc)
+            if (visited.contains(work.head)) findConnectedStates(timeout, work.tail, edges, visited, acc)
             else {
                 val head = work.head
                 val next = edges(head)
                 // Prepend the edges and work upfront the respective lists (assume next to be much shorter than work/acc).
-                findConnectedStates(next.map(_._2).toList ++ work.tail, edges, visited + head, next.map(t => (head, t._1, t._2)).toList ++ acc)
+                findConnectedStates(timeout, next.map(_._2).toList ++ work.tail, edges, visited + head, next.map(t => (head, t._1, t._2)).toList ++ acc)
             }
         }
     
@@ -128,10 +130,60 @@ class IncrementalConcurrentModular[Exp, A <: Address, V, T, TID <: ThreadIdentif
                                        vstore,                                              // Store.
                                        Map.empty)                                           // Graph edges.
     
-        val result: OuterLoopState = outerLoop(oState)
+        val result: OuterLoopState = outerLoop(timeout, oState)
         theStore = result.store
         // After running the result, possibly unreachable edges may need to be filtered out.
-        Graph[G, State, Transition].empty.addEdges(findConnectedStates(result.threads.values.flatten.toList, result.edges))
+        Graph[G, State, Transition].empty.addEdges(findConnectedStates(timeout, result.threads.values.flatten.toList, result.edges))
     
     }
+    
+    def runWithLabels[G](program: Exp, timeout: Timeout.T)(implicit ev: Graph[G, State, Transition]): G = {
+    
+        /** Filters out unreachable graph components that may result from invalidating edges. */
+        @scala.annotation.tailrec
+        def findConnectedStates(timeout: Timeout.T, tid: TID, work: List[State], edges: Edges, visited: Set[State] = Set.empty, acc: GraphEdges = List.empty, labels: Map[Int, Int] = Map().withDefaultValue(0)): (GraphEdges, Map[Int, Int]) = {
+            if (timeout.reached || work.isEmpty) return (acc, labels)
+            if (visited.contains(work.head)) findConnectedStates(timeout, tid, work.tail, edges, visited, acc, labels)
+            else {
+                val head = work.head
+                val next = edges(head)
+                val lbs = next.foldLeft(labels){(lbs, e) =>
+                    val i = e._1.l.toInt
+                    lbs + (i -> (lbs(i) + 1))}
+                // Prepend the edges and work upfront the respective lists (assume next to be much shorter than work/acc).
+                findConnectedStates(timeout, tid, next.map(_._2).toList ++ work.tail, edges, visited + head, next.map(t => (head, t._1, t._2)).toList ++ acc, lbs)
+            }
+        }
+        
+        val cc      :          KAddr = HaltKontAddr
+        val env     : Environment[A] = Environment.initial[A](sem.initialEnv)
+        val control :        Control = ControlEval(program, env)
+        val kstore  :         KStore = Store.empty[KAddr, Set[Kont]](t)
+        val time    :              T = timestamp.initial("")
+        val tid     :            TID = allocator.allocate(program, time)
+        val state   :          State = State(tid, control, cc, time, kstore)
+        val threads :        Threads = Map(tid -> Set(state)).withDefaultValue(Set.empty)
+        val vstore  :         VStore = Store.initial[A, V](t, sem.initialStore)(lattice)
+        val oState  : OuterLoopState = OuterLoopState(threads,                              // Threads.
+            List(state),                                         // Worklist.
+            Deps(Map.empty.withDefaultValue(Set.empty),          // Join dependencies.
+                Map.empty.withDefaultValue(Set.empty),          // Read dependencies.
+                Map.empty.withDefaultValue(Set.empty)),         // Write dependencies.
+            Map.empty.withDefaultValue(lattice.bottom),     // Return values.
+            vstore,                                              // Store.
+            Map.empty)                                           // Graph edges.
+    
+        val result: OuterLoopState = outerLoop(timeout, oState)
+        val (edg, labels): (GraphEdges, List[(TID, Map[Int, Int])]) =
+            result.threads.keySet.foldLeft((List[(State, Transition, State)](),
+                List[(TID, Map[Int, Int])]()))
+            { case ((edg, lab), td) =>
+                val (e, l): (GraphEdges, Map[Int, Int]) = findConnectedStates(timeout, td, result.threads(td).toList, result.edges)
+                (edg ++ e, (td, l) :: lab)
+            }
+        theLabels = labels.foldLeft((List[(Int, Map[Int, Int])](), 0)){case ((acc, n), (_, mp)) => ((n, mp) :: acc, n + 1)}._1
+        Graph[G, State, Transition].empty.addEdges(edg)
+        
+    }
+    
 }
