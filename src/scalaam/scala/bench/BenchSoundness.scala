bf7d9f1edf6f495be2bcb50281affa29a630120d
Jens Van der Plas <jevdplas@vub.be>
2019-06-07 23:20:49 +0200

    BenchSoundness now writes results to file.

--- src/scalaam/scala/bench/BenchSoundness.scala
+++ src/scalaam/scala/bench/BenchSoundness.scala
@@ -1,18 +1,36 @@
 package scala.bench
 
+import java.io.{BufferedWriter, FileWriter}
+import java.text.SimpleDateFormat
+import java.util.{Calendar, Date}
+
+import au.com.bytecode.opencsv.CSVWriter
 import scalaam._
 import scalaam.core._
 
 object BenchSoundness extends App {
-
+    
     import scalaam.bench.BenchConfig.Prelude._
     import scalaam.bench.BenchConfig._
     import scalaam.language.scheme._
     
     val latt = SchemeLattice[Sem.lattice.L, SchemeExp, Sem.address.A]
     
+    val now: Date = Calendar.getInstance().getTime
+    val format: SimpleDateFormat = new SimpleDateFormat("_yyyy-MM-dd-HH'h'mm")
+    val output: String = "./Results_Soundness" + format.format(now) + ".txt"
+    
+    val out = new BufferedWriter(new FileWriter(output))
+    val writer = new CSVWriter(out)
+    
+    def displayf(text: String): Unit = {
+        writer.writeNext(text)
+        writer.flush()
+    }
+    
     def forFile(file: String, atPrelude: Prelude): Unit = {
         display("\n" ++ file ++ " ")
+        displayf("\n" ++ file ++ " ")
         try {
             val f = scala.io.Source.fromFile(file)
             // Add the necessary preludes to the file contents.
@@ -45,11 +63,11 @@ object BenchSoundness extends App {
         }
         if (mod.keySet != inc.keySet) {
             if (mod.keySet.subsetOf(inc.keySet)) {
-                display(s"Abstract has observed extra variables: ${inc.keySet.diff(mod.keySet)}\n")
+                displayf(s"Abstract has observed extra variables: ${inc.keySet.diff(mod.keySet)}\n")
             } else {
-                display("!!!SOUNDNESS PROBLEM!!!\n")
+                displayf("!!!SOUNDNESS PROBLEM!!!\n")
                 /* If the concrete execution observed variables not observed in the abstract, the abstract is not sound! */
-                display(s"Concrete has observed extra variables: ${mod.keySet.diff(inc.keySet)}\n")
+                displayf(s"Concrete has observed extra variables: ${mod.keySet.diff(inc.keySet)}\n")
                 return /* And we can directly abort */
             }
         }
@@ -58,14 +76,15 @@ object BenchSoundness extends App {
             val modval = mod(id)
             val incval = inc(id)
             if (incval == modval) {
-//                display(s"$id: full precision! ($incval)\n")
+                //                displayf(s"$id: full precision! ($incval)\n")
             } else if (!latt.subsumes(incval, modval)) {
-                display(s"$id: SOUNDNESS PROBLEM, inferred $incval while concrete shows $modval\n")
+                displayf(s"$id: SOUNDNESS PROBLEM, inferred $incval while concrete shows $modval\n")
             } else {
-                display(s"$id: overapproximative, inferred as $incval while best abstraction is $modval\n")
+                displayf(s"$id: overapproximative, inferred as $incval while best abstraction is $modval\n")
             }
         })
     }
-
-  benchmarks.foreach(Function.tupled(forFile))
+    
+   benchmarks.foreach(Function.tupled(forFile))
+    writer.close()
 }
