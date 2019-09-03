960cf1a891d67c5f5aaa7569e571d027df36fd0b
Quentin Stiévenart <quentin.stievenart@gmail.com>
2019-09-03 14:27:47 +0200

    Rename new msort to mqsort, restore old msort

---
+++ test/Atomlang/VMCAI2020/mqsort.scm
@@ -0,0 +1,76 @@
+;; Multithreaded merge-sort
+(define (merge-lists xs ys)
+  (if (null? xs)
+      ys
+      (if (null? ys)
+          xs
+          (if (< (car xs) (car ys))
+              (cons (car xs) (merge-lists (cdr xs) ys))
+              (cons (car ys) (merge-lists xs (cdr ys)))))))
+
+(define (take l n)
+  (if (or (= n 0) (null? l))
+      '()
+      (cons (car l) (take (cdr l) (- n 1)))))
+
+(define (drop l n)
+  (if (or (= n 0) (null? l))
+      l
+      (drop (cdr l) (- n 1))))
+
+(define (merge-sort l)
+  (let ((len (length l)))
+    (if (< len 2)
+        l
+        (if (= len 2)
+            (merge-lists (list (car l)) (list (cadr l)))
+            (let ((first-half (future (merge-sort (take l (quotient len 2)))))
+                  (second-half (future (merge-sort (drop l (quotient len 2))))))
+              (merge-lists (deref first-half) (deref second-half)))))))
+
+(define (append l m) ; Do not remove this non-mutable definition!
+  (if (null? l)
+      m
+      (cons (car l) (append (cdr l) m))))
+
+;; Quick-sort code comes from https://stackoverflow.com/questions/43474008/scheme-implementing-a-quick-sort
+(define (partition pred xs)
+   (let part ((ps '()) (ns '()) ; Initial "positives" `ps`, and "negatives" `ns`
+              (xs2 xs) )
+      (if (null? xs2)
+         (cons ps ns)             ; Returning pair of lists
+         (let ((x (car xs2)))     ; Memoization of `(car lst)`
+            (if (pred x)
+               (part (cons x ps) ns (cdr xs2))
+               (part ps (cons x ns) (cdr xs2)) )))))
+
+(define (quick-sort xs)
+   (if (null? xs) '()
+      (let* ((x (car xs))
+             (pn (partition               ; Memoization of `partition`
+                    (lambda (y)
+                       (< y x) )
+                    (cdr xs) ))
+             (left (future (quick-sort (car pn)))) ; Extracting positives from pair
+             (right (future (quick-sort (cdr pn))))) ; negatives
+         (append (deref left)
+                 (append (list x)                  ; Pivot
+                         (deref right))))))
+
+
+(define (sorted? l)
+  (if (or (null? l) (null? (cdr l)))
+      #t
+      (if (<= (car l) (cadr l))
+          (sorted? (cdr l))
+          #f)))
+
+(define (generate-list size)
+  (if (= size 0)
+      '()
+      (cons (random 100) (generate-list (- size 1)))))
+
+(define L (generate-list (random 100)))
+(sorted? (merge-sort L))
+(sorted? (quick-sort L))
+(equal? (merge-sort L) (quick-sort L))
--- test/Atomlang/VMCAI2020/msort.scm
+++ test/Atomlang/VMCAI2020/msort.scm
@@ -1,4 +1,4 @@
-;; Multithreaded merge-sort
+;; Multithrded merge-sort
 (define (merge-lists xs ys)
   (if (null? xs)
       ys
@@ -28,36 +28,6 @@
                   (second-half (future (merge-sort (drop l (quotient len 2))))))
               (merge-lists (deref first-half) (deref second-half)))))))
 
-(define (append l m) ; Do not remove this non-mutable definition!
-  (if (null? l)
-      m
-      (cons (car l) (append (cdr l) m))))
-
-;; Quick-sort code comes from https://stackoverflow.com/questions/43474008/scheme-implementing-a-quick-sort
-(define (partition pred xs)
-   (let part ((ps '()) (ns '()) ; Initial "positives" `ps`, and "negatives" `ns`
-              (xs2 xs) )
-      (if (null? xs2)
-         (cons ps ns)             ; Returning pair of lists
-         (let ((x (car xs2)))     ; Memoization of `(car lst)`
-            (if (pred x)
-               (part (cons x ps) ns (cdr xs2))
-               (part ps (cons x ns) (cdr xs2)) )))))
-
-(define (quick-sort xs)
-   (if (null? xs) '()
-      (let* ((x (car xs))
-             (pn (partition               ; Memoization of `partition`
-                    (lambda (y)
-                       (< y x) )
-                    (cdr xs) ))
-             (left (future (quick-sort (car pn)))) ; Extracting positives from pair
-             (right (future (quick-sort (cdr pn))))) ; negatives
-         (append (deref left)
-                 (append (list x)                  ; Pivot
-                         (deref right))))))
-
-
 (define (sorted? l)
   (if (or (null? l) (null? (cdr l)))
       #t
@@ -72,4 +42,3 @@
 
 (define L (generate-list (random 100)))
 (sorted? (merge-sort L))
-(sorted? (quick-sort L))
