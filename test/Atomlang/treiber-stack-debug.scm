9686c8525c6b71380378b28dd39cee9dc712c8d3
Jens Van der Plas <jevdplas@vub.ac.be>
2019-09-19 08:31:32 +0200

    Further reduction of size

--- test/Atomlang/treiber-stack-debug.scm
+++ test/Atomlang/treiber-stack-debug.scm
@@ -1,21 +1,9 @@
 ; Treiber stack as an example of a lock-free data structure using atoms.
-(define (map f l)
-  (if (null? l)
-    '()
-      (cons (f (car l)) (map f (cdr l)))))
 
-(define (push stk el) (compare-and-set! stk #t (cons el #t)))
+; NOTE: ARTIFICIAL ORDERING IMPOSED ON OUTER LOOP ATOMANALYSIS
 
-(define (loop stk n f) (loop stk (f stk n) f))
+(define (loop stk f) (loop stk (f stk) f))
 
-(define (main nops)
-  (let* ((stacks (cons (atom '()) '()))
-         (push1-ops (map (lambda (stack)
-                          (future (loop stack nops (lambda (s n) (push s n) (- n 1)))))
-                         stacks))
-         (pop1-ops (map (lambda (stack)
-                         (future (loop stack nops (lambda (s n) #t))))
-                        stacks)))
-    #t))
+(future (loop (atom '()) (lambda (s) (compare-and-set! (atom '()) #t #t))))
+(future (loop (atom '()) (lambda (s) #t)))
 
-(main 0)
